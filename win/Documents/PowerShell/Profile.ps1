d914252d76c7f6e1b89017ccbb47f95a5d6e4a9e
robertobernabe <florian.schaeffeler+github@gmail.com>
2022-10-28 09:02:11 +0200

    Added tab completion for python pip.

--- win/Documents/PowerShell/Profile.ps1
+++ win/Documents/PowerShell/Profile.ps1
@@ -1,5 +1,14 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
 
+# Needed for midnight commander
+$Env:LANG = "en_EN.UTF8-8"
+
+function Get-VsCodeDiff {
+    & code -d $args
+}
+
+New-Alias -Name codediff -Value Get-VsCodeDiff -Force -Option AllScope
+
 function Get-SvnIgnoreTxt($DirPath = ".") {
     $svnAllNotAdded = (svn status) | select-string -pattern '\?'
     Set-Content -Path (Join-Path $Env:Tmp ignore-(New-Guid).txt) -Value $svnAllNotAdded
@@ -15,3 +24,25 @@ function Clear-WindowsDarkTheme() {
     New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
 }
 
+# pip powershell completion start
+if ((Test-Path Function:\TabExpansion) -and -not `
+    (Test-Path Function:\_pip_completeBackup)) {
+    Rename-Item Function:\TabExpansion _pip_completeBackup
+}
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+    if ($lastBlock.StartsWith("python.exe -m pip ")) {
+        $Env:COMP_WORDS = $lastBlock
+        $Env:COMP_CWORD = $lastBlock.Split().Length - 1
+        $Env:PIP_AUTO_COMPLETE = 1
+        (& python.exe -m pip).Split()
+        Remove-Item Env:COMP_WORDS
+        Remove-Item Env:COMP_CWORD
+        Remove-Item Env:PIP_AUTO_COMPLETE
+    }
+    elseif (Test-Path Function:\_pip_completeBackup) {
+        # Fall back on existing tab expansion
+        _pip_completeBackup $line $lastWord
+    }
+}
+# pip powershell completion end
